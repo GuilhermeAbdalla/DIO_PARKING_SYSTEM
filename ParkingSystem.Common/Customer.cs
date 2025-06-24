ee93e6827c37848c010bea7fbc6f86d2add3a63e
Guilherme_Abdalla <abdalla.seleman@hotmail.com>
2025-06-23 23:29:10 -0300

    Added the main method and started Customer Register

---
+++ ParkingSystem.Common/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingSystem.Common;
+
+public struct Customer
+{
+    public int customerId;
+    public string customerName;
+    public string customerDocument;
+    public string customerPhoneNumber;
+    public string customerEmail;
+}
